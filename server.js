875dbd518d897ff637cdba8e8d67f5b530642d16
Matthew Brown <MatthJBrown@googlemail.com>
2024-04-05 18:10:27 +0100

    opened more bandwidth

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(express.bodyParser({limit: '50mb'}));
 
 let tokenStore = new NodeCache();
 
